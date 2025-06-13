4d11d520969d36d2c9429fec25dd7fad6431e14f
ngenoclinton <clintonkipkoechngeno@gmail.com>
2025-06-13 11:16:08 +0300

    implement component import

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css"
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
+import Header from "../components/header"
+import Footer from "../components/Footer"
 import AnimationProvider from "@/components/AnimationProvider"
 import Script from "next/script"
 // import LoadingProvider from "@/components/loading-provider"
